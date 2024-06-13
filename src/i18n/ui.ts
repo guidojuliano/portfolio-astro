c905def4c77e63b36f2b971b629b7b1de1aaa65e
Guido Juliano Ubaldi <guidojulianou@gmail.com>
2024-06-13 12:14:02 -0300

    Initial commit

---
+++ src/i18n/ui.ts
@@ -0,0 +1,88 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+    "nav.experience": "Experience",
+    "nav.projects": "Projects",
+    "nav.contact": "Contact",
+    "home.title": "Hi! I'm Guido Juliano",
+    "home.work": "Web Developer.",
+    "home.description":
+      "Specialized in creating high-quality web applications.",
+    "about.title": "About Me",
+    "about.description1":
+      "I'm a web developer with a passion for creating beautiful and functional websites. I am studying Systems Engineering and pursuing various courses and certifications.",
+    "about.description2":
+      "I am a continuously learning professional who adapts quickly to new work environments. I enjoy working in teams and possess effective communication skills.",
+    "about.description3":
+      "I am seeking opportunities to join an IT team in a company where I can develop professionally and contribute my skills.",
+    "experience.title": "Work Experience",
+    "experience.job": "Frontend Developer",
+    "experience.company": "Nuevo Banco del Chaco S.A.",
+    "experience.duration": "September, 2022",
+    "experience.description":
+      "Development with React and TypeScript, using agile methodologies, and assisting with project designs and decision-making. ",
+    "experience.job2": "Full Stack Developer",
+    "experience.company2": "Ministerio de Educación",
+    "experience.duration2": "January, 2020 - September, 2022",
+    "experience.description2":
+      "I developed with Python and Django, and also assisted in creating a User CRUD system with PostgreSQL. ",
+    "projects.title": "Projects",
+    "projects.project1": "Quix - Ecommerce",
+    "projects.description1":
+      "Ecomerse website built with NextJS where I implemented data fetching and data mutation with GraphQL, cookie management, middleware implementation for routes control, AI implementation to create a chat-bot, SEO, performance analysis and more",
+    "projects.project2": "Arkanoid",
+    "projects.description2": "Classic arcade game built with JavaScript",
+    "projects.project3": "Artworks",
+    "projects.description3":
+      "List of artworks fetched from an API displayed in a paginated table using Material-UI components",
+    "projects.button": "View on Github",
+  },
+  es: {
+    "nav.home": "Inicio",
+    "nav.about": "Sobre mi",
+    "nav.experience": "Experiencia",
+    "nav.projects": "Proyectos",
+    "nav.contact": "Contacto",
+    "home.title": "¡Hola! Soy Guido Juliano",
+    "home.work": "Desarrollador web.",
+    "home.description":
+      "Especializado en la creación de sitios web de alta calidad.",
+    "about.title": "Sobre mi",
+    "about.description1":
+      "Soy un desarrollador web con pasión por crear sitios web atractivos y funcionales. Estudio Ingeniería en Sistemas y estoy realizando diferentes cursos y certificaciones.",
+    "about.description2":
+      "Soy un profesional en constante aprendizaje que se adapta rápidamente a nuevos entornos de trabajo. Disfruto trabajar en equipo y poseo habilidades de comunicación efectivas.",
+    "about.description3":
+      "Busco oportunidades para unirme a un equipo de TI en una empresa donde pueda desarrollarme profesionalmente y aportar mis habilidades.",
+    "experience.title": "Experiencia Laboral",
+    "experience.job": "Desarrollador Frontend",
+    "experience.company": "Nuevo Banco del Chaco S.A.",
+    "experience.duration": "Septiembre, 2022",
+    "experience.description":
+      "Desarrollo con React y TypeScript, utilizando metodologías ágiles y asistiendo en el diseño de proyectos y toma de decisiones.",
+    "experience.job2": "Desarrollador Full Stack",
+    "experience.company2": "Ministerio de Educación",
+    "experience.duration2": "Enero, 2020 - Septiembre, 2022",
+    "experience.description2":
+      "Desarrollé con Python y Django, y también asistí en la creación de un sistema CRUD de usuarios con PostgreSQL.",
+    "projects.title": "Proyectos",
+    "projects.project1": "Quix - Ecommerce",
+    "projects.description1":
+      "Sitio de Ecomerse construido con NextJS en donde implemente data fetching y data mutation con GraphQL, manejo de cookies, implementación de middleware para control de rutas, implementación de IA para crear un chat-bot, SEO, análisis de performance, entre otras cosas",
+    "projects.project2": "Arkanoid",
+    "projects.description2":
+      "Juego clasico de arcade implementado con JavaScript",
+    "projects.project3": "Artworks",
+    "projects.description3":
+      "Lista de obras de arte obtenidas de una API mostrada en una tabla paginada usando componentes Material-UI",
+    "projects.button": "Ver en Github",
+  },
+} as const;
