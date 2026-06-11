41180101d52626969fcef80e3c67e5b54a5e8622
Guido Juliano Mazzuchini Ubaldi <guidojulianou@gmail.com>
2026-06-11 10:41:46 -0300

    Se agrega modulo especial del mundial

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,17 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: {
+    server: {
+      proxy: {
+        '/api/games': {
+          target: 'https://worldcup26.ir',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api\/games/, '/get/games')
+        }
+      }
+    }
+  }
 });
